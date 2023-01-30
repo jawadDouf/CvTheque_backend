b07f8d65372a34df5cbd4c091eb3c898ef1e9194
jawadDouf <jdoufare2001@gmail.com>
2023-01-30 11:11:28 +0100

    build the application env

---
+++ src/main/java/com/example/cvthequebackend/CvthequeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cvthequebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvthequeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvthequeBackendApplication.class, args);
+	}
+
+}
